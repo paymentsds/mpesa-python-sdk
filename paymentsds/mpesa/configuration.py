c90b21207f36b08f91063426766c501f1bd84cf2
Edson Michaque <edson@michaque.com>
2020-07-19 13:25:17 +0200

    Remove useless methods from configuration.py

--- paymentsds/mpesa/configuration.py
+++ paymentsds/mpesa/configuration.py
@@ -24,24 +24,3 @@ class Configuration:
           self.environment = Environment.from_url(value)
         else:
           self.__dict__[key] = value
-
-  def build_default_operations(self)
-    pass
-
-  def build_default_headers(self):
-    pass
-
-  def generate_headers(self):
-    pass
-
-  def generate_authorization_header(self):
-    pass
-
-  def generate_url(self, operation):
-    pass
-
-  def is_valid(self):
-    pass
-
-  def generate_base_url(self, operation):
-    pass
