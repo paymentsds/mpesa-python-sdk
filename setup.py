4e0eb0b47f1ae85831b9b992e163f396c4d6ce33
Edson Michaque <edson@michaque.com>
2020-07-19 13:33:53 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name='paymentsds-mpesa',
+    version='0.1',
+    description='MPesa Python SDK',
+    license='Apache-2.0',
+    author='Edson Michaque',
+    author_email='edson@michaque.com',
+    keywords=['MPesa'],
+    url='https://github.com/paymentsds/mpesa-python-sdk'
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
