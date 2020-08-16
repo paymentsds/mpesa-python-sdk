3c6ba95f9bc5577202bea87b6406fbec8d35fba3
Edson Michaque <edson@michaque.com>
2020-08-16 20:26:34 +0200

    Bump 0.1.0-alpha-1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='paymentsds-mpesa',
-    version='0.1',
+    version='0.1.0-aplha-1',
     description='MPesa Python SDK',
     license='Apache-2.0',
     author='Edson Michaque',
