3382f477be71d5d3145295f26d939e72ba420e7e
Edson Michaque <edson@michaque.com>
2020-08-16 22:38:43 +0200

    Bump version 0.1a4

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='paymentsds-mpesa',
-    version='0.1a1',
+    version='0.1a4',
     description='MPesa Python SDK',
     license='Apache-2.0',
     author='Edson Michaque',
     author_email='edson@michaque.com',
     keywords=['MPesa'],
-    url='https://github.com/paymentsds/mpesa-python-sdk'
+    url='https://github.com/paymentsds/mpesa-python-sdk',
+    packages=[
+        "paymentsds",
+        "paymentsds/mpesa"
+    ]
 )
 
 if __name__ == '__main__':
